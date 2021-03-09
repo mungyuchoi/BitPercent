2cf62f6945063975e2b7bfb9ded471ae29a9cc95
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2021-03-09 09:43:14 +0900

    Initial commit

---
+++ app/src/main/java/com/moon/bitpercent/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moon.bitpercent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
